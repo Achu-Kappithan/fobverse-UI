0bed3caf8cff669a9bdb091e0efa378afd930db9
Achu-kappithan <achukappithan4236@gmail.com>
2025-07-21 18:18:28 +0530

    feat: implement guard to restrict logged-in users from accessing public routes and improve auth guard logic

---
+++ src/app/shared/guards/auth_gurds/auth-gurd-guard.ts
@@ -0,0 +1,35 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { UserRegisterService } from '../../../features/auth/services/auth.service';
+import { combineLatest, filter, map, switchMap, take } from 'rxjs';
+
+export const authGurdGuard: CanActivateFn = (route, state) => {
+
+  const _authService = inject(UserRegisterService);
+  const router = inject(Router);
+
+ return _authService.isUserLoaded.pipe(
+    filter(loaded => loaded === true), 
+    take(1),
+    switchMap(() => {
+      return combineLatest([
+        _authService.admin$,
+        _authService.company$,
+        _authService.candidate$
+      ]).pipe(
+        take(1), 
+        map(([adminUser, companyUser, candidateUser]) => {
+          const isLoggedIn = !!adminUser || !!companyUser || !!candidateUser;
+
+          if (isLoggedIn) {
+            console.log('authGurdGuard: User is logged in. Allowing access.');
+            return true; 
+          } else {
+            console.log('authGurdGuard: User is NOT logged in. Redirecting to /login');
+            return router.createUrlTree(['/login']); 
+          }
+        })
+      );
+    })
+  );
+};
