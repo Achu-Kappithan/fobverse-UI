41e935df3f1a2e6e8cb13f7e0d6cfeb0c7958630
Achu-kappithan <achukappithan4236@gmail.com>
2025-08-20 12:55:22 +0530

    feat(candidate) Implement jobview in the candidate joblisting

---
+++ src/app/features/candidate/components/layout-jolist/layout-jolist.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout-jolist',
+  imports: [RouterModule],
+  templateUrl: './layout-jolist.html',
+  styleUrl: './layout-jolist.css'
+})
+export class LayoutJolist {
+
+}
