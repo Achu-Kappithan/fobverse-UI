c2c3a2704f3c11b90ad5cbdcc96dc87c6342a90f
Achu-kappithan <achukappithan4236@gmail.com>
2026-02-13 14:13:32 +0530

    refactor (refactor) update code for deployement

--- src/env/environment.ts
+++ src/env/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
   apiUrl: '/api',
   cloudinaryUrl: 'https://res.cloudinary.com/dl9iuhkmq',
   cloudinaryBaseUrl: "https://res.cloudinary.com/dl9iuhkmq/image/upload",
-  socketUrl: 'http://localhost:3007',
+  socketUrl: 'https://fobverse-api.onrender.com',
   googleClientId: '304451399030-adooleficc1nd40qr3igngh4d2dmbc8v.apps.googleusercontent.com',
   nominatimUrl: 'https://nominatim.openstreetmap.org/search?format=json&limit=5&q='
 };
